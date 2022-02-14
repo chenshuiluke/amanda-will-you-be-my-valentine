30c0edb7ee876d4dcbf042cc20fccb49b91c9ff8
Luke Chen Shui <chenshuiluke@gmail.com>
2022-02-13 21:32:05 -0500

    Got basic question

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Heart } from './Heart';
+function App() {
+  return (
+    <>
+    <Heart></Heart>
+    <h1><span className='red'>Amanda</span>, will you be my valentine?</h1>
+    </>
+  );
+}
+
+export default App;
