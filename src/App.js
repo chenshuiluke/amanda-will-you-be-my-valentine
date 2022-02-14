bae0457211cbedda2044c95a529fde1367fa4774
Luke Chen Shui <chenshuiluke@gmail.com>
2022-02-13 22:10:24 -0500

    Added assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css';
 import { Heart } from './Heart';
-function App() {
+import 'animate.css';
+import { Component, useEffect, useState } from 'react';
+
+const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+
+
+
+function importAll(r) {
+  let images = {};
+   r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images
+}
+debugger
+
+export function App() {
+  const [displayAnswer, setDisplayAnswer] = useState(false)
+  const [displayRest, setDisplayRest] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDisplayAnswer(true)
+    }, 5000)
+  }, [])
   return (
     <>
     <Heart></Heart>
     <h1><span className='red'>Amanda</span>, will you be my valentine?</h1>
+
+    {displayAnswer && <>
+      <button class="button animate__fadeIn" onClick={() => setDisplayRest(true)}>
+        Yes
+        <div class="button__horizontal"></div>
+        <div class="button__vertical"></div>
+      </button>
+    </>}
+
+    {displayRest && <>
+      {
+        Object.values(images).map((image) => {
+          return <img src={image} />
+        })
+      }
+    </>}
+    
     </>
   );
 }
